b3f2220ebe885821fa8c5b54f582d377823f8cfa
brandarizfacundo-ops <brandarizfacundo@gmail.com>
2026-05-08 12:29:58 -0300

    Add files via upload

--- change.py
+++ change.py
@@ -1,5 +1,22 @@
 def change():
-    """Lee un gasto y el dinero recibido, calcula el vuelto
-    y lo separa en pesos (parte entera) y centavos.
-    """
-    pass
+    print("Ingresar gasto:")
+    gasto = float(input())
+    print(gasto)
+    print("Dinero recibido")
+    dinero = int(input())
+    print(dinero)
+
+    vuelto = dinero - gasto
+
+    decimal = int(round(vuelto * 100))
+    pesos = decimal // 100
+    centavos = decimal % 100
+
+
+    print()
+    print("Vuelto")
+    print()
+    print("Pesos:")
+    print(pesos)
+    print("Centavos:")
+    print(int(centavos))
